77b0a4990e1ece4c3b30f17a7a5dc1d740640bea
j.white450 <j.white450@DEN.coloradotech.edu>
2018-10-17 22:17:26 -0600

    added table

--- games.sql
+++ games.sql
@@ -1,1 +1,23 @@
 --/this is the end of this game-- 
+
+
+CREATE TABLE Product(
+	[ProductID] [int] NOT NULL,
+	[GameID]	[int],
+	[ConsoleID] [int], 
+	[AccessoryID] [int] 
+	CONSTRAINT pk_Product PRIMARY KEY (ProductID)
+) 
+GO
+
+INSERT Product (ProductID, ConsoleID, AccessoryID)
+	VALUES		(425, 200, 4638,223);
+
+
+
+CREATE TABLE Game(
+	GameName	VARCHAR (50),
+	Manufacture	VARCHAR (50),
+	Quantity	INT,
+	ConsoleType	VARCHAR (50)
+)
